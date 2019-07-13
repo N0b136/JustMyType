32a94598003ba3ed174b063f3183d3755dfa45ee
n0b136 <50467142+N0b136@users.noreply.github.com>
2019-07-13 15:56:22 -0500

    Highlited keys when pressed, displays targets

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 $(document).ready(function(){
     let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+    let i = 0;
+    let kpi = 0;
+    let currSen = sentences[i];
 
+    $('#sentence').text(currSen);
     $('#keyboard-upper-container').hide();
+    $('#target-letter').text(currSen.charAt(kpi));
 
     $(document).keydown(function(event){
-        if (event.which == 16) {
-        $('#keyboard-upper-container').show();
-        $('#keyboard-lower-container').hide();
+        if (event.which == 16) { 
+            $('#keyboard-lower-container').hide();
+            $('#keyboard-upper-container').show();
+        }
+        
     });
     $(document).keyup(function(event){
         if (event.which == 16) {
         $('#keyboard-upper-container').hide();
         $('#keyboard-lower-container').show();
-        };
+        }
+        $('.key').removeClass('hl-letter');
+        kpi ++;
+        $('#target-letter').text(currSen.charAt(kpi));
     });
-
+    $(document).keypress(function (event){
+        $('#'+event.which).addClass('hl-letter');
+    });
+    
+    
+    
 });
